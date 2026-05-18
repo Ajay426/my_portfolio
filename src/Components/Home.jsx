81ac6a40bc1ecc315d8e1dcf99800583ab24f6c6
Ajay Mahato <ajaymahato7155@gmail.com>
2026-05-18 13:57:50 +0530

    resume update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -59,10 +59,12 @@ export default function Home() {
                      whileHover={{ scale: 1.07, y: -2 }}
                      whileTap={{ scale: 0.9, y: 1 }}
                      transition={{ type: "spring", stiffness: 300, damping: 7 }} variant="outlined" sx={{ marginRight: "15px", backgroundColor: "rgb(3,57,108)", color: "rgb(255, 255, 255)" }} onClick={() => navigate("/contact")} >Hire me</MotionButton>
-                  <a href='/Ajay_Mahato_resume.pdf' download> <MotionButton
-                     whileHover={{ scale: 1.07, y: -2 }}
-                     whileTap={{ scale: 0.9, y: 1 }}
-                     transition={{ type: "spring", stiffness: 300, damping: 7 }} variant="outlined">Download CV <ArrowDownwardIcon /></MotionButton></a>
+                  <a href={`${import.meta.env.BASE_URL}Ajay_Mahato_resume.pdf`} download target="_blank" rel="noreferrer">
+                     <MotionButton
+                        whileHover={{ scale: 1.07, y: -2 }}
+                        whileTap={{ scale: 0.9, y: 1 }}
+                        transition={{ type: "spring", stiffness: 300, damping: 7 }} variant="outlined">Download CV <ArrowDownwardIcon /></MotionButton>
+                  </a>
                </Box>
 
 
